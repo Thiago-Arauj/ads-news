0644ad597f84a13bb90228bf33416354a8c86b49
Thiago Araujo <thiagorodrigues12372@gmail.com>
2025-05-09 15:13:37 -0300

    feat: added .env.example and models for news

--- apps/nyt_api/models.py
+++ apps/nyt_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from apps.core.models import BaseModel
+
 
 # Create your models here.
+class News(BaseModel):
+    section = models.CharField(
+        max_length=255,
+        verbose_name="Seção",
+        db_index=True
+    )
+    title = models.CharField(max_length=255, verbose_name="Titulo")
+    abstract = models.TextField(verbose_name="Resumo")
+    news_url = models.URLField(verbose_name="Link da matéria", unique=True)
+    image_url = models.URLField(verbose_name="Url da imagem")
+    published_date = models.DateTimeField(
+        verbose_name="Data de publicação",
+        db_index=True
+    )
