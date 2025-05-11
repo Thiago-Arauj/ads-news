98f9552642998513ffa5eabae2aa19208ebf337e
Paulo Cleidson <167731152+Paulo-Cleidson@users.noreply.github.com>
2025-05-10 22:04:39 -0300

    bug fix update

--- ads_news/urls.py
+++ ads_news/urls.py
@@ -18,10 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from apps.nyt_api.views import root
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', root),
-    path('', include('apps.core.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('apps.core.urls')),
+] 
+
+#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
